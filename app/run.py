fc4d4fbaa5586066a8feb4861eb1db8ad81b62a5
ElenaStiv <elenaselena3@gmail.com>
2026-05-17 13:43:15 +0300

    Add files via upload

--- app/run.py
+++ app/run.py
@@ -1,1 +1,7 @@
+# run the app at localhost:5000
 
+from __init__ import app
+
+if __name__ == '__main__':
+    # Το debug=True σημαίνει ότι αν αλλάζεις τον κώδικα, ο server θα κάνει refresh μόνος του!
+    app.run(debug=True, port=5000)
